2f1aceb56ee06895f8e3354854071bd63e419d85
Jean Humblot <jhumblot1@gmail.com>
2021-05-22 13:21:43 +0900

    add analytics

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,8 @@
-/* eslint-disable react/jsx-no-target-blank */
-/* eslint-disable jsx-a11y/anchor-has-content */
-/* eslint-disable jsx-a11y/control-has-associated-label */
-/* eslint-disable jsx-a11y/anchor-is-valid */
 import { useEffect } from 'react';
 import { gsap } from 'gsap';
 import ScrollTrigger from 'gsap/ScrollTrigger';
+import firebase from 'firebase';
+import 'firebase/analytics';
 
 import './App.css';
 import Hero from './components/Hero';
@@ -13,6 +11,10 @@ import Anc from './components/Anc';
 import FakeLink from './components/FakeLink';
 
 gsap.registerPlugin(ScrollTrigger);
+
+if (process.env.NODE_ENV !== 'development') {
+  firebase.analytics();
+}
 
 function App() {
   useEffect(() => {
@@ -88,7 +90,7 @@ function App() {
         <div className="container">
           <h3 className="thanks-heading">Thanks for scrolling through.</h3>
           <div className="madeby">
-            <a href="https://webflow.com/website/AirPods-Pro-Website?rfsn=3423979.8e540a" target="_blank" className="open-in-webflow-link w-inline-block">
+            <a href="https://webflow.com/website/AirPods-Pro-Website?rfsn=3423979.8e540a" target="_blank" rel="noreferrer" className="open-in-webflow-link w-inline-block">
               <div className="text-block">Based on the workflow project of Moritz Petersen</div>
             </a>
           </div>
